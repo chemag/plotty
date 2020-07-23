28a8937718bf845aec95bfd3ed2c5ef471dfe754
Chema Gonzalez <chemag@fb.com>
2020-07-23 12:57:45 -0700

    plotty: histogram: add support for multiple input files
    Tested:
    ```
    $ cat /tmp/h1.txt
    1
    2
    3
    2
    1
    2
    3
    4
    3
    2
    1
    2
    $ cat /tmp/h2.txt
    3
    3
    4
    4
    5
    4
    3
    4
    5
    4
    
    $ ./plotty-histogram.py -i /tmp/h1.txt --fmt 'r' --label 'hist1' -i /tmp/h2.txt --fmt 'g' --label 'hist2' /tmp/out.png
    No handles with labels found to put in legend.
    No handles with labels found to put in legend.
    output is /tmp/out.png
    ```

--- plotty-histogram.py
+++ plotty-histogram.py
@@ -15,29 +15,33 @@ import math
 import sys
 
 
+DEFAULT_COLORS = ['r', 'g', 'b', 'c', 'm', 'y', 'k']
+DEFAULT_FMT = DEFAULT_COLORS
+
 default_values = {
     'debug': 0,
     'title': '--title',
-    'label': '',
     'col': 0,
     # number of bins for the histogram
     'nbins': 50,
     'sigma': None,
     'filter': None,
-    'fmt': 'ro',
     'sep': None,
     'xlabel': '--xlabel',
     'ylabel': '--ylabel',
     'add_mean': False,
     'add_median': False,
+    'label': [],
+    'fmt': [],
     'xlim': ['-', '-'],
     'ylim': ['-', '-'],
-    'infile': None,
+    'infile': [],
     'outfile': None,
 }
 
 # filter ops
 VALID_OPS = 'eq', 'ne'
+
 
 def read_data(infile, options):
     # open infile
@@ -75,51 +79,73 @@ def read_data(infile, options):
                     new_data.append(row)
         data = new_data
 
-    if options.col == -1:
-        x = range(len([row for row in data if row]))
-    else:
-        x = [float(row.split(sep)[options.col]) for row in data if row]
+    xlist = []
+    for i, row in enumerate(data):
+        if not row:
+            # empty row
+            continue
+        # get x component
+        if options.col == -1:
+            # use line number
+            x = i
+        else:
+            # use column value
+            x = float(row.split(sep)[options.col])
+        # append values
+        xlist.append(x)
+
     # remove values way over the average
     if options.sigma is not None:
-        total_x = sum(x)
-        mean_x = total_x / len(x)
-        stddev_x = math.sqrt(sum((i - mean_x) ** 2 for i in x) / (len(x) - 1))
+        total_x = sum(xlist)
+        mean_x = total_x / len(xlist)
+        stddev_x = math.sqrt(sum((i - mean_x) ** 2 for i in xlist) /
+                             (len(xlist) - 1))
         min_value = mean_x - (stddev_x * options.sigma)
         max_value = mean_x + (stddev_x * options.sigma)
-        new_x = [i for i in x if i > min_value and i < max_value]
-        if (len(x) - len(new_x)) / len(x) > 0.1:
+        new_xlist = [i for i in xlist if i > min_value and i < max_value]
+        if (len(xlist) - len(new_xlist)) / len(xlist) > 0.1:
+            dropped_pct = 100. * (len(xlist) - len(new_xlist)) / len(xlist)
             print('Ignoring sigma removal of outliers (at sigma: %f would be '
                   'dropping %f%% of the values' % (
-                      options.sigma, 100. * (len(x) - len(new_x)) / len(x)))
+                      options.sigma, dropped_pct))
         else:
             if options.debug > 0:
                 print('Removing %i of %i values sigma: %f stddev: %f '
                       'range: [%f, %f]' % (
-                          len(x) - len(new_x), len(x), options.sigma,
+                          len(xlist) - len(new_xlist),
+                          len(xlist), options.sigma,
                           stddev_x, min_value, max_value))
-            x = new_x
-    return x
+            xlist = new_xlist
+    return xlist
 
 
-def create_graph(x, options):
+def create_graph_begin(options):
     # create figure
-    fig, ax = plt.subplots(figsize=(8, 4))
+    fig, ax1 = plt.subplots(figsize=(8, 4))
+    # tidy up the figure
+    ax1.grid(True)
+    ax1.legend(loc='right')
+    ax1.set_title(options.title)
+    ax1.set_xlabel(options.xlabel)
+    ax1.set_ylabel('Frequency of the value')
+    return ax1
 
+
+def create_graph_draw(ax1, xlist, fmt, label, options):
     # plot the non-cumulative histogram
-    n, bins, patches = ax.hist(x, options.nbins, histtype='step')
+    n, bins, patches = ax1.hist(xlist, options.nbins, histtype='step',
+                                color=fmt)
 
     # add median and average
     if options.add_median:
-        plt.axvline(np.median(x), color='r', linestyle='dotted', linewidth=1)
+        plt.axvline(np.median(xlist), color=fmt, linestyle='dotted',
+                    linewidth=1)
     if options.add_mean:
-        plt.axvline(np.mean(x), color='k', linestyle='dashed', linewidth=1)
+        plt.axvline(np.mean(xlist), color=fmt, linestyle='dashed', linewidth=1)
 
-    # tidy up the figure
-    ax.grid(True)
-    ax.legend(loc='right')
-    ax.set_title(options.title)
-    ax.set_xlabel(options.xlabel)
-    ax.set_ylabel('Frequency of the value')
+
+def create_graph_end(ax1, options):
+    _ = ax1.legend(loc='upper right')
 
     # set xlim/ylim
     if options.xlim[0] != '-':
@@ -177,10 +203,14 @@ def get_options(argv):
                         dest='ylabel', default=default_values['ylabel'],
                         metavar='YLABEL',
                         help='use YLABEL x label',)
-    parser.add_argument('--label', action='store',
+    parser.add_argument('--fmt', action='append',
+                        dest='fmt', default=default_values['fmt'],
+                        metavar='FMT',
+                        help='use FMT format(s)',)
+    parser.add_argument('--label', action='append',
                         dest='label', default=default_values['label'],
                         metavar='LABEL',
-                        help='use LABEL label',)
+                        help='use LABEL label(s)',)
     parser.add_argument('--add-mean', action='store_const',
                         dest='add_mean', const=True,
                         default=default_values['add_mean'],
@@ -195,10 +225,10 @@ def get_options(argv):
     parser.add_argument('--ylim', action='store', type=str, nargs=2,
                         dest='ylim', default=default_values['ylim'],
                         metavar=('bottom', 'top'),)
-    parser.add_argument('infile', type=str,
+    parser.add_argument('-i', '--infile', action='append',
                         default=default_values['infile'],
                         metavar='input-file',
-                        help='input file',)
+                        help='input file(s)',)
     parser.add_argument('outfile', type=str,
                         default=default_values['outfile'],
                         metavar='output-file',
@@ -225,17 +255,27 @@ def get_options(argv):
 def main(argv):
     # parse options
     options = get_options(argv)
-    # get infile/outfile
-    if options.infile == '-':
-        options.infile = sys.stdin
+    # get infile(s)/outfile
+    options.infile = [(sys.stdin if name == '-' else name) for name in
+                      options.infile]
     if options.outfile == '-':
         options.outfile = sys.stdout
     # print results
-    if options.debug > 2:
+    if options.debug > 0:
         print(options)
+
     # create the graph
-    x = read_data(options.infile, options)
-    create_graph(x, options)
+    x_data = []
+    for infile in options.infile:
+        x_data.append(read_data(infile, options))
+
+    ax1 = create_graph_begin(options)
+    for index, xlist in enumerate(x_data):
+        fmt = (options.fmt[index] if index < len(options.fmt) else
+               DEFAULT_FMT[index])
+        label = options.label[index] if index < len(options.label) else ''
+        create_graph_draw(ax1, xlist, fmt, label, options)
+    create_graph_end(ax1, options)
 
 
 if __name__ == '__main__':
