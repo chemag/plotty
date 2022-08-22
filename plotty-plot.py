287998c328272c43c90095b54d67e6e227e91c1c
Chema Gonzalez <chemag@fb.com>
2022-08-22 13:42:16 -0700

    plotty: plot: move prefilter code to a separate file
    This is part of the process of breaking the code in several files.

--- plotty-plot.py
+++ plotty-plot.py
@@ -20,6 +20,7 @@ import os
 from scipy.optimize import curve_fit
 import sys
 
+prefilter_lib = importlib.import_module('prefilter')
 utils = importlib.import_module('utils')
 
 
@@ -312,114 +313,6 @@ def parse_csv(raw_data, sep, header):
     return column_names, lines
 
 
-# prefilter processing
-class Prefilter:
-    # prefilter ops
-    VALID_FILTER_OPS = 'eq', 'ne', 'gt', 'ge', 'lt', 'le'
-    VALID_BOOL_OPS = 'and', 'or'
-
-    def __init__(self, string):
-        self.item_list = []
-        self.bool_list = []
-        self.string = string
-        if string is None:
-            # empty prefilter
-            return
-        # PREFILTER-SPEC := ITEM [BOP ITEM]*
-        # ITEM := FCOL FOP FVAL
-        # where
-        #   ITEM: a 3-element function returning a boolean
-        #   FCOL: column ID (number or column name)
-        #   FOP: valid prefilter operation
-        #   FVAL: value
-        #   BOP: valid bool operation
-        # notes
-        #   * BOP evaluation is left-to-right. This means "A and B or C"
-        #     is evaluated as "(A and B) or C"
-        #   * Adding parenthesis/NOT operation to BOP will require a more
-        #     complex approach (tree structure). Not worth it yet.
-        f = string.split()
-        if len(f) == 0:
-            # empty prefilter
-            self.string = None
-            return
-        # break the prefilter in group of 3x items
-        assert len(f) % 4 == 3, f'incorrect number of elements in "{string}"'
-        item = f[0:0+3]
-        self.assert_item(*item)
-        self.item_list.append(item)
-        i = 3
-        while i < len(f):
-            bop = f[i]
-            self.assert_bool(bop)
-            self.bool_list.append(bop)
-            item = f[i+1:i+4]
-            self.assert_item(*item)
-            self.item_list.append(item)
-            i += 4
-
-    def assert_bool(self, bop):
-        assert bop in self.VALID_BOOL_OPS, (
-            f'invalid bool operation ("{bop}") in prefilter. '
-            f'Options: {self.VALID_BOOL_OPS}')
-
-    def assert_item(self, fcol, fop, fval):
-        assert fop in self.VALID_FILTER_OPS, (
-            f'invalid prefilter op ("{fop}") in "{fcol} {fop} {fval}". '
-            f'Options: {self.VALID_FILTER_OPS}')
-
-    def fix_columns(self, column_names):
-        if self.string is None:
-            return
-        new_item_list = []
-        for fcol, fop, fval in self.item_list:
-            if utils.is_int(fcol):
-                fcol = int(fcol)
-            else:
-                # look for named columns
-                assert fcol in column_names, (
-                    'error: invalid fcol name: "%s"' % fcol)
-                fcol = column_names.index(fcol)
-            new_item_list.append([fcol, fop, fval])
-        self.item_list = new_item_list
-
-    def match_item(self, item, val_list):
-        fcol, fop, fval = item
-        lval = val_list[fcol]
-        # implement eq and ne
-        if fop in ('eq', 'ne'):
-            if ((fop == 'eq' and lval != fval) or
-                    (fop == 'ne' and lval == fval)):
-                return False
-        # implement gt, ge, lt, le
-        elif fop in ('gt', 'ge', 'lt', 'le'):
-            # make sure line val and prefilter val are numbers
-            lval = float(lval)
-            fval = float(fval)
-            if ((fop == 'ge' and lval < fval) or
-                    (fop == 'gt' and lval <= fval) or
-                    (fop == 'le' and lval > fval) or
-                    (fop == 'lt' and lval >= fval)):
-                return False
-        return True
-
-    def match_line(self, line, sep):
-        if self.string is None:
-            return True
-        val_list = line.split(sep)
-        # run all the items
-        item_vals = [self.match_item(item, val_list) for item in
-                     self.item_list]
-        # coalesce them using the boolean ops
-        i = 0
-        ret = item_vals[i]
-        for bop in self.bool_list:
-            bool_op = bool.__and__ if bop == 'and' else bool.__or__
-            i += 1
-            ret = bool_op(ret, item_vals[i])
-        return ret
-
-
 def filter_lines(lines, sep, prefilter):
     new_lines = []
     for line in lines:
@@ -458,7 +351,7 @@ def parse_line(line, i, sep, xcol, ycol, sep2, xcol2, ycol2, xfactor):
 
 
 def parse_data(raw_data, ycol, xshift_local, yshift_local, prefilter, options):
-    prefilter = Prefilter(prefilter)
+    prefilter = prefilter_lib.Prefilter(prefilter)
     sep = options.sep if options.sep != '' else None
     xcol = options.xcol
     xcol2 = options.xcol2
@@ -993,10 +886,10 @@ def get_options(argv):
     # check the filters
     if options.prefilter is not None:
         for prefilter in options.prefilter:
-            Prefilter(prefilter)
+            prefilter_lib.Prefilter(prefilter)
     if options.batch_prefilter is not None:
         for prefilter in options.batch_prefilter:
-            Prefilter(prefilter)
+            prefilter_lib.Prefilter(prefilter)
     # check there is an input file
     assert options.infile or options.batch_infile, (
         'error: must provide valid input file')
---
+++ prefilter.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+"""prefilter.py: Prefilter implementation.
+"""
+
+
+import importlib
+
+utils = importlib.import_module('utils')
+
+
+# prefilter processing
+class Prefilter:
+    # prefilter ops
+    VALID_FILTER_OPS = 'eq', 'ne', 'gt', 'ge', 'lt', 'le'
+    VALID_BOOL_OPS = 'and', 'or'
+
+    def __init__(self, string):
+        self.item_list = []
+        self.bool_list = []
+        self.string = string
+        if self.string is None:
+            # empty prefilter
+            return
+        # PREFILTER-SPEC := ITEM [BOP ITEM]*
+        # ITEM := FCOL FOP FVAL
+        # where
+        #   ITEM: a 3-element function returning a boolean
+        #   FCOL: column ID (number or column name)
+        #   FOP: valid prefilter operation
+        #   FVAL: value
+        #   BOP: valid bool operation
+        # notes
+        #   * BOP evaluation is left-to-right. This means "A and B or C"
+        #     is evaluated as "(A and B) or C"
+        #   * Adding parenthesis/NOT operation to BOP will require a more
+        #     complex approach (tree structure). Not worth it yet.
+        f = self.string.split()
+        if len(f) == 0:
+            # empty prefilter
+            self.string = None
+            return
+        # break the prefilter in group of 3x items
+        assert len(f) % 4 == 3, f'incorrect num of elements in "{self.string}"'
+        item = f[0:0+3]
+        self.assert_item(*item)
+        self.item_list.append(item)
+        i = 3
+        while i < len(f):
+            bop = f[i]
+            self.assert_bool(bop)
+            self.bool_list.append(bop)
+            item = f[i+1:i+4]
+            self.assert_item(*item)
+            self.item_list.append(item)
+            i += 4
+
+    def assert_bool(self, bop):
+        assert bop in self.VALID_BOOL_OPS, (
+            f'invalid bool operation ("{bop}") in prefilter. '
+            f'Options: {self.VALID_BOOL_OPS}')
+
+    def assert_item(self, fcol, fop, fval):
+        assert fop in self.VALID_FILTER_OPS, (
+            f'invalid prefilter op ("{fop}") in "{fcol} {fop} {fval}". '
+            f'Options: {self.VALID_FILTER_OPS}')
+
+    def fix_columns(self, column_names):
+        if self.string is None:
+            return
+        new_item_list = []
+        for fcol, fop, fval in self.item_list:
+            if utils.is_int(fcol):
+                fcol = int(fcol)
+            else:
+                # look for named columns
+                assert fcol in column_names, (
+                    'error: invalid fcol name: "%s"' % fcol)
+                fcol = column_names.index(fcol)
+            new_item_list.append([fcol, fop, fval])
+        self.item_list = new_item_list
+
+    def match_item(self, item, val_list):
+        fcol, fop, fval = item
+        lval = val_list[fcol]
+        # implement eq and ne
+        if fop in ('eq', 'ne'):
+            if ((fop == 'eq' and lval != fval) or
+                    (fop == 'ne' and lval == fval)):
+                return False
+        # implement gt, ge, lt, le
+        elif fop in ('gt', 'ge', 'lt', 'le'):
+            # make sure line val and prefilter val are numbers
+            lval = float(lval)
+            fval = float(fval)
+            if ((fop == 'ge' and lval < fval) or
+                    (fop == 'gt' and lval <= fval) or
+                    (fop == 'le' and lval > fval) or
+                    (fop == 'lt' and lval >= fval)):
+                return False
+        return True
+
+    def match_line(self, line, sep):
+        if self.string is None:
+            return True
+        val_list = line.split(sep)
+        # run all the items
+        item_vals = [self.match_item(item, val_list) for item in
+                     self.item_list]
+        # coalesce them using the boolean ops
+        i = 0
+        ret = item_vals[i]
+        for bop in self.bool_list:
+            bool_op = bool.__and__ if bop == 'and' else bool.__or__
+            i += 1
+            ret = bool_op(ret, item_vals[i])
+        return ret
